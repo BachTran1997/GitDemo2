08aa6704925c80c72fd6bf2a0070f60530c19688
BachTran <trannguyenbach1997@gmail.com>
2016-11-02 17:55:34 +0700

    a

---
+++ gitDemo/src/gitDemo/GuiDemo.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package gitDemo;
+
+import javax.swing.JFrame;
+
+/**
+ * @author BachTran
+ *
+ */
+public class GuiDemo extends JFrame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		GuiDemo wMain = new GuiDemo();
+		wMain.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		wMain.setVisible(true);
+		
+		
+
+	}
+
+}
